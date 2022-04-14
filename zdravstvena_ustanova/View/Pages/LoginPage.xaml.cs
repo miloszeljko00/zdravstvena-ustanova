8a6f7824a5e98290ded45c1cf054a599a6e59709
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-04-14 02:21:08 +0200

    Implemented login screen and basic home screen for manager (only WPF!)

---
+++ zdravstvena_ustanova/View/Pages/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using zdravstvena_ustanova.View.Pages.ManagerPages;
+
+namespace zdravstvena_ustanova.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ManagerMainPage());
+        }
+    }
+}
---
+++ zdravstvena_ustanova/View/Pages/ManagerPages/ManagerMainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zdravstvena_ustanova.View.Pages.ManagerPages
+{
+    /// <summary>
+    /// Interaction logic for ManagerHome.xaml
+    /// </summary>
+    public partial class ManagerMainPage : Page
+    {
+        public ManagerMainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void roomsButton_Click(object sender, RoutedEventArgs e)
+        {
+            homeButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            reportsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            warehouseButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            roomsButton.Background = new SolidColorBrush(Color.FromRgb(97, 164, 188));
+            itemsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            drugsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            pollsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            accountButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+
+
+            ManagerMain.Content = new ManagerRoomsPage();
+        }
+    }
+}
