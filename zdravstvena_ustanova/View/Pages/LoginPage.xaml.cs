9abb84998277d111174b137a85b3057c69933f8a
Nikola Holjevac <holjevac.ra11.2019@uns.ac.rs>
2022-05-05 23:29:10 +0200

    updated patient sequence diagram

--- zdravstvena_ustanova/View/Pages/LoginPage.xaml.cs
+++ zdravstvena_ustanova/View/Pages/LoginPage.xaml.cs
@@ -36,11 +36,7 @@ namespace zdravstvena_ustanova.View.Pages
         {
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Password;
-            //string username = "velja";
-            //string password = "velja";
 
-            username = "upravnik";
-            password = "upravnik";
 
             if (username == null || password == null) return;
 
