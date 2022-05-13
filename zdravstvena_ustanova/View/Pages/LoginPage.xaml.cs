b4632889c15ef8144425347655dcbd895f3afb4f
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-05-13 04:19:22 +0200

    implemented filters for ItemType in InventoryOverviewPage for selected room

--- zdravstvena_ustanova/View/Pages/LoginPage.xaml.cs
+++ zdravstvena_ustanova/View/Pages/LoginPage.xaml.cs
@@ -37,8 +37,8 @@ namespace zdravstvena_ustanova.View.Pages
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Password;
 
-            username = "velja";
-            password = "velja";
+            username = "upravnik";
+            password = "upravnik";
 
             if (username == null || password == null) return;
 
