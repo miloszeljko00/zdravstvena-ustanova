b377b314543921a6ed892e42b7463c0aabe455ba
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-06-01 22:07:18 +0200

    Implemented wpf page for RoomsOccupancy

--- zdravstvena_ustanova/View/Pages/ManagerPages/ManagerMainPage.xaml.cs
+++ zdravstvena_ustanova/View/Pages/ManagerPages/ManagerMainPage.xaml.cs
@@ -173,5 +173,19 @@ namespace zdravstvena_ustanova.View.Pages.ManagerPages
 
             ManagerMain.Content = new PollsOverviewPage();
         }
+
+        private void RoomReportsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            homeButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            reportsButton.Background = new SolidColorBrush(Color.FromRgb(97, 164, 188));
+            warehouseButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            roomsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            itemsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            drugsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            pollsButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+            accountButton.Background = new SolidColorBrush(Color.FromRgb(247, 226, 226));
+
+            ManagerMain.Content = new RoomsOccupancyPage();
+        }
     }
 }
--- zdravstvena_ustanova/View/Pages/ManagerPages/RoomsOccupancyPage.xaml.cs
+++ zdravstvena_ustanova/View/Pages/ManagerPages/RoomsOccupancyPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using zdravstvena_ustanova.Model;
 
 namespace zdravstvena_ustanova.View.Pages.ManagerPages
 {
@@ -20,9 +22,23 @@ namespace zdravstvena_ustanova.View.Pages.ManagerPages
     /// </summary>
     public partial class RoomsOccupancyPage : Page
     {
+        public ObservableCollection<Room> Rooms { get; set; }
         public RoomsOccupancyPage()
         {
             InitializeComponent();
+            DataContext = this;
+            var app = Application.Current as App;
+            Rooms = new ObservableCollection<Room>();
+            var rooms = app.RoomController.GetAll();
+            foreach (var room in rooms)
+            {
+                Rooms.Add(room);
+            }
+        }
+
+        private void GenerateReportButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
