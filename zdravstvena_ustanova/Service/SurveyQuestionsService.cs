681a4be86abd0e4af2f25953f4112e704f614a7b
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2022-06-01 18:57:05 +0200

    SurveyQuestionsService error solved

--- zdravstvena_ustanova/Service/SurveyQuestionsService.cs
+++ zdravstvena_ustanova/Service/SurveyQuestionsService.cs
@@ -5,10 +5,11 @@ using zdravstvena_ustanova.Repository;
 using System.Linq;
 using zdravstvena_ustanova.Model.Enums;
 using zdravstvena_ustanova.Repository.RepositoryInterface;
+using zdravstvena_ustanova.Service.ServiceInterface;
 
 namespace zdravstvena_ustanova.Service
 {
-    public class SurveyQuestionsService : ISurveyQuestionsRepository
+    public class SurveyQuestionsService : ISurveyQuestionsService
     {
         private readonly ISurveyQuestionsRepository _surveyQuestionsRepository;
         private readonly IScheduledAppointmentRepository _scheduledAppointmentRepository;
